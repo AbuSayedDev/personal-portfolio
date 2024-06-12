cc01072935a74edeab56ec7f175801e81b91e684
Abu Sayed <abusayed285235@gmail.com>
2024-06-12 21:00:23 +0600

    About us  section add

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,14 @@ $(".owl-carousel").owlCarousel({
   autoplayHoverPause: true,
   responsive: {
     0: {
-      items: 1,
-      nav: true,
-    },
-    600: {
       items: 2,
       nav: false,
+      dots: false,
+    },
+    600: {
+      items: 3,
+      nav: false,
+      dots: false,
     },
     1000: {
       items: 5,
